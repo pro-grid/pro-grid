5a51749307ff398df57dfa56ea4c0b12e4140fc3
Austin Pray <austin@austinpray.com>
2014-01-18 15:43:44 -0600

    back up and running removed all refs to socket
    fixes compile errro

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('proGridApp')
-  .controller('MainCtrl', function ($scope, socket) {
+  .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
