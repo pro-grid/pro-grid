c54ac37aa0a5039dd9764aa781adfaacb6c57506
Ridwan Hoq <ridwanhoq@gmail.com>
2014-01-18 15:13:56 -0600

    A lot of JSHint errors rn. @austinpray pls help

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,14 +1,26 @@
 'use strict';
 
 angular.module('proGridApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, socket, Socketioservice),  {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
     $scope.dimensions = 16;
+
     $scope.generateGrid = function(num) {
       return new Array(num);   
     }
+
+    $scope.gridClicked = function(row, col) {
+      socket.emit('grid:clicked');
+      // TODO: Respond to user click on the grid (by animation etc.)
+    };
+
+    // Socket listeners
+    socket.on('grid:clicked', function(row, col) {
+      gridClicked(row, col);
+    });
+
   });
