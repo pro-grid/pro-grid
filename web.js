0706cebbd23f8191a0de7a5d31b27dd53cdc11d3
Austin Pray <austin@austinpray.com>
2014-01-18 18:08:13 -0600

    prepares for heroku deploy

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
 
-app.listen(9001);
+var express = require('express');
+var app = express()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
 
-function handler (req, res) {
-  //lol
-}
+var port = process.env.PORT || 9001;
+server.listen(port);
+
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/dist/index.html');
+});
 
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
