d270fad36f5a2849b4908ea62527c16cbc2f75d0
Darren Cattle <cattle.darren@gmail.com>
2014-01-20 21:03:09 -0600

    random color function added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,14 +3,23 @@
 angular.module('proGridApp')
   .controller('MainCtrl', function ($scope) {
     var socket = io.connect();
-    
+    var getRandomColor = function() {
+      var letters = '0123456789ABCDEF'.split('');
+      var color = '#';
+      for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.round(Math.random() * 15)];
+      }
+      return color;
+    }
+    var UserColor = getRandomColor();
     socket.on('update', function (data) {
       console.log(data);
       var selector = ".col_" + data.row + "_" + data.col;
       var el = document.querySelector(selector);
       if(el.className.indexOf("black") > -1) {
         el.className = el.className.replace("black", "");
-        console.log("if");
+        el.style.backgroundColor=UserColor;
+        console.log(UserColor);
       } else {
         el.className = el.className + " black";
         console.log("else");
@@ -31,6 +40,7 @@ angular.module('proGridApp')
     $scope.gridClicked = function(row, col) {
       //alert(row + " " + col);
       socket.emit('clicked', { row: row, col: col });
-    };
 
+    };
+    
   });
