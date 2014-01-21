99f8eb4cb6357163bd455eb94f0d3837c0f3e822
Austin Pray <austin@austinpray.com>
2014-01-20 22:20:59 -0600

    adds random color service

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,18 +4,12 @@ angular.module('proGridApp')
   .controller('MainCtrl', ['$scope', 'Randomcolor', function ($scope, Randomcolor) {
     var socket = io.connect();
     // Socket listener for updating grid
-    console.log("random " + Randomcolor.new());
+    var userColor = Randomcolor.new();
     socket.on('update', function (data) {
       console.log(data);
       var selector = ".col_" + data.row + "_" + data.col;
       var el = document.querySelector(selector);
-      if(el.className.indexOf("black") > -1) {
-        el.className = el.className.replace("black", "");
-        
-      } else {
-        el.className = el.className + " black";
-        console.log("else");
-      }
+      el.style.backgroundColor = userColor;
     });
     
     $scope.awesomeThings = [
