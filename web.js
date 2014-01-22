dbf047d4f8e8129fc5efe1e032ab778c1bd693f1
Jason Fletcher <jfletch@gmail.com>
2014-01-22 03:20:59 -0500

    - Clean up server side code. - Add support for server side throttling (by managing last update time across each individual socket connection). - Add support for tracking the number of abuse attempts (for possible addition of disconnect/ban later).

--- web.js
+++ web.js
@@ -1,6 +1,11 @@
+/******************************************************
+*                  SERVER INITIALIZATION              *
+******************************************************/
+
+// NewRelic is required if the production env is Heroku
 if(process.env.NEW_RELIC_APP_NAME) {
   require('newrelic');
-} // requires new relic if the production env is heroku
+}
 
 var gridDimensions = 32;
 var express = require('express');
@@ -9,14 +14,23 @@ var app = express()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 
+// Setup port for frontend usage
+var port = process.env.PORT || 9001;
+server.listen(port);
 
-function validateData(data, dimensions) {
-  // yeah so what it's a long return statement why you talkin shit
-  return validator.isInt(data.row) && validator.isInt(data.col) && validator.isHexColor(data.color) && data.row < dimensions && data.col < dimensions;
-}
+app.use(express.static(__dirname + '/dist'));
 
-// instantiate grid array
-// this happens once per server boot
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/dist/index.html');
+});
+
+
+
+/******************************************************
+*                   DATA INITIALIZATION               *
+******************************************************/
+
+// Instantiate an empty grid
 var grid = [];
 for(var y = 0; y < gridDimensions; y++) {
   grid.push(new Array(gridDimensions));
@@ -29,12 +43,31 @@ for(var y = 0; y < gridDimensions; y++) {
   }
 }
 
-/* server startup checks. If any of these fail the server will not run. 
- * the validateData function in order to start the server. 
+
+
+/******************************************************
+*                   DATA VALIDATION                   *
+******************************************************/
+
+// Check grid cell is valid, within dimensions, and the submitted color is valid
+function validateData(data, dimensions) {
+  return validator.isInt(data.row) &&
+    validator.isInt(data.col) &&
+    validator.isHexColor(data.color) &&
+    data.row < dimensions &&
+    data.col < dimensions;
+}
+
+/*
+ * Server Startup Checks.
+ *
+ * If any of these fail the server will not run.
+ * the validateData function in order to start the server.
  */
 (function serverTests() {
   //test validateData
   console.log("info: Startup tests engage");
+
   var testData = {
     row: 10000,
     col: "notvalid",
@@ -46,38 +79,68 @@ for(var y = 0; y < gridDimensions; y++) {
   } else {
     console.log("PASSED: validateData is nominal");
   }
+
   if(!grid) {
     console.error("FAILED: grid is not instantiated properly");
     process.exit(1);
   } else {
     console.log("PASSED: grid instantiation nominal");
   }
+
 }());
 
 
-var port = process.env.PORT || 9001;
-server.listen(port);
 
-app.use(express.static(__dirname + '/dist'));
+/******************************************************
+*                SOCKET COMMUNICATION                 *
+******************************************************/
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/dist/index.html');
-});
-
+// Called whenever a new socket connection is formed by client
 io.sockets.on('connection', function (socket) {
+  // Send current state of grid
   socket.emit('server ready', { gridArray: grid });
-  //Socket listener for user click
+
+  // Received whenever user clicks a tile on the frontend
   socket.on('clicked', function (data) {
-    if(validateData(data, gridDimensions)) {
-      var gridCol = grid[data.row][data.col];
-      if(gridCol.color == '') {
-        gridCol.color = data.color;
+
+    // Grab last_updated_at to check 100ms have passed since last update
+    socket.get('last_updated_at', function(err, timestamp) {
+      current_datetime = (new Date().getTime() / 1000);
+      if (timestamp && ((current_datetime - timestamp) <= 0.100)) {
+
+        // Increment the invalid update counter
+        socket.get('invalid_updates', function( err, invalid_updates) {
+          socket.set('invalid_updates', ((!invalid_updates) ? 1 : invalid_updates + 1));
+        });
+        socket.emit('naughty', { message: "must wait 100ms between update requests" });
+
+      // Valid update request time, verify validity
       } else {
-        gridCol.color = '';
+        // Change color of grid cell if a valid change
+        if (validateData(data, gridDimensions)) {
+          var gridCol = grid[data.row][data.col];
+
+          // If colored, make blank (white). Otherwise, make blank cell player's color
+          if(gridCol.color == '') {
+            gridCol.color = data.color;
+          } else {
+            gridCol.color = '';
+          }
+
+          // Set current update time BEFORE broadcast
+          last_updated_at = (new Date().getTime() / 1000);
+          socket.set('last_updated_at', last_updated_at);
+
+          // Reset invalid updates counter
+          socket.set('invalid_updates', 0);
+
+          // Broadcast the color update to all connections other than sender
+          socket.broadcast.emit('update', gridCol);
+        } else {
+          // Invalid update request, respond to connection with error message
+          socket.emit('naughty', { message: "data validation did not pass"});
+        }
       }
-      socket.broadcast.emit('update', gridCol);
-    } else {
-      socket.emit('naughty', { message: "data validation did not pass"});
-    }
+    });
   });
 });
