d5024376e2afe4a23659bb64d724a626b22af628
Ridwan Hoq <ridwanhoq@gmail.com>
2014-02-01 13:44:41 -0600

    moved ApiKeyHandler to it's own module

--- web.js
+++ web.js
@@ -8,7 +8,6 @@ if(process.env.NEW_RELIC_APP_NAME) {
 var express = require('express')
   , app = express()
   , async = require('async')
-  , uuid = require('node-uuid')
   , validator = require('validator')
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
@@ -54,85 +53,7 @@ function updateGrid (client, data) {
   });
 }
 
-// Store for ApiKeys
-var ApiKeys = require('memory-cache');
-ApiKeys.debug(true);
-
-// Create, verify, update and delete
-// Api Keys
-// ApiKeyHandler
-var ApiKeyHandler = function (client, key, callback) { // Create
-  console.log('called ApiKeyHandler');
-  var self = this;
-  this.client = client;
-  this.clientSession = {};
-  this.key = key;
-  async.series({
-    checkIfExists: function(callback) {
-      self.clientSession = ApiKeys.get(client.id);
-      callback(!self.clientSession || null);
-    },
-    throttle: function(callback) {
-      var last_check = self.clientSession.createTime;
-      var compareTime = process.hrtime(last_check);
-      compareTime = (compareTime[0] * 1e9 + compareTime[1]); // convert to nanoseconds
-      var rate = 7; // unit: clicks
-      var per  = 1000000000; // unit: nanoseconds (1 second)
-      self.clientSession.allowance += compareTime * (rate / per);
-      console.log('time diff: ' + compareTime);
-      if (self.clientSession.allowance > rate) {
-        self.clientSession.allowance = rate; // discard extra tokens
-      }
-      if (self.clientSession.allowance < 1.0) {
-        callback("rate limited");
-      }
-      else {
-        self.clientSession.allowance -= 1.0;
-        callback(null, self.clientSession.allowance);
-      }
-      
-    }
-  },
-  function(err, results) {
-    console.log('ApiKeyHandler Initialization: \n result: %s \n data: %s', err, JSON.stringify(results));
-    if(!err) {
-      ApiKeyHandler.newKey(self.client, self.clientSession, callback);
-    } else {
-      self.client.disconnect();
-    }
-  });
-};
-
-ApiKeyHandler.verify = function(key) {
-  return validator.isUUID(key, 4);
-};
-
-ApiKeyHandler.newKey = function(client, data, callback) {
-  var Client = client.id || client;
-  var key = uuid.v4();
-  data = (data || {}) // data is optional
-  ApiKeys.put(
-    Client,
-    {
-      key: key,
-      createTime: process.hrtime(),
-      allowance: data.allowance || 7
-    },
-    3600000); // value sits there for 1 hour if left alone
-  console.log('saved');
-  if(callback !== undefined) {
-    console.log('trying to send fresh api key');
-    client.emit('fresh api key', { apiKey: key });
-    callback(client);
-  }
-};
-
-ApiKeyHandler.delete = function(client, callback) {
-  ApiKeys.del(client);
-  if(callback !== undefined) {
-    callback(null, true);
-  }
-};
+var ApiKeyHandler = require('./apikeyhandler');
 
 // core app logic
 var port = process.env.PORT || 9001;
@@ -164,7 +85,7 @@ io.set('log level', 1);
 // describe client connection   
 io.sockets.on('connection', function (socket) {
   ApiKeyHandler.newKey(socket, null, function (socket) { // DIRTY FIX ME
-    console.log('api keys registered:\n' + ApiKeys.size());
+    console.log('api keys registered:\n' + ApiKeyHandler.ApiKeys.size());
     socket.emit('server ready', { gridArray: grid });
     //Socket listener for user click
     socket.on('clicked', function (data) {
@@ -179,7 +100,7 @@ io.sockets.on('connection', function (socket) {
     });
     socket.on('disconnect', function() {
       ApiKeyHandler.delete(socket.id);
-      console.log('api keys registered:\n' + ApiKeys.size());
+      console.log('api keys registered:\n' + ApiKeyHandler.ApiKeys.size());
     });
   });
 });
