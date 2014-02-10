d8bfcca7e90a0e7fe358b31be266edf1e947c856
Darren Cattle <cattle.darren@gmail.com>
2014-02-09 19:18:02 -0600

    adds 8 color mod

--- app/scripts/services/randomcolor.js
+++ app/scripts/services/randomcolor.js
@@ -1,14 +1,11 @@
 'use strict';
-
+//selects a random color from 8 options of hexadecimal strings from clrs.cc
 angular.module('proGridApp')
   .factory('Randomcolor', function Randomcolor() {
     return {
       new: function() {
-        var letters = '0123456789ABCDEF'.split('');
-        var color = '#';
-        for (var i = 0; i < 6; i++ ) {
-          color += letters[Math.round(Math.random() * 15)];
-        }
+        var colorset = ['#0074D9','#FF851B','#01FF70','#FFDC00','#AAAAAA','#FF4136','#F012BE','#111111'];
+        var color = colorset[Math.floor(Math.random() * 8)];
         return color;
       }
     };
