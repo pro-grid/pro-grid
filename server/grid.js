fca6cb3e04c560d2362c205dd44616708ef0164a
Ridwan Hoq <ridwanhoq@gmail.com>
2014-02-20 02:45:57 -0600

    Got a basic implementation of persistence working (#66) - When the Grid object is instntiated, it checks to see if we have a previous state - If we do, it goes through and constructs a request to Redis to get those values and puts them in the right places in the grid - Otherwise, it sets a flag so that the next time the grid is instantiated, it will know to load the state from Redis - Currently, storing individual clicks to the grid is not async (which will be addressed in the next couple of commits)

--- server/grid.js
+++ server/grid.js
@@ -1,21 +1,39 @@
-'use strict'
+'use strict';
+
+var redis = require('redis')
+  , redisClient = redis.createClient()
+  , async = require('async');
 
 var Grid = function(gridDimensions) {
-  this.gridMatrix = [];
+  this.gridDimensions = gridDimensions;
+  var self = this;
+  self.gridMatrix = [];
   for(var y = 0; y < gridDimensions; y++) {
-    this.gridMatrix.push(new Array(gridDimensions));
+    self.gridMatrix.push(new Array(gridDimensions));
     for(var x = 0; x < gridDimensions; x++) {
-      this.gridMatrix[y][x] = {
+      self.gridMatrix[y][x] = {
         row: y,
         col: x,
         color: ''
       };
     }
   }
+  redisClient.get('gridSaved', function(err, reply) {
+    if (reply) {
+      console.log('THE GRID EXISTS');
+      console.log(reply);
+      self.loadGridFromRedis(gridDimensions);
+    }
+    else {
+      redisClient.set('gridSaved', 'HELLA_SAVED', redis.print);
+      console.log('THE GRID DID NOT EXIST PRIOR TO THIS MOMENT');
+    }
+  });
 };
 
 Grid.prototype.updateGrid = function(client, data) {
   var gridCol = this.updateGridMatrix(data);
+  this.updateRedis(data);
   console.log('updating grid');
   client.broadcast.emit('update', gridCol, function() {
     console.log('updated grid');
@@ -30,6 +48,36 @@ Grid.prototype.updateGridMatrix = function(data) {
     gridCol.color = '';
   }
   return gridCol;
+};
+
+Grid.prototype.updateRedis = function(data) {
+  var key = data.row + '-' + data.col;
+  var value = data.color;
+  console.log('saved ' + value + ' to redis for ' + key);
+  redisClient.set(key, value, redis.print);
+};
+
+Grid.prototype.loadGridFromRedis = function(gridDimensions) {
+  var keyList = [];
+  var self = this;
+  for(var y = 0; y < gridDimensions; y++) {
+    for(var x = 0; x < gridDimensions; x++) {
+      var key = y + '-' + x;
+      keyList.push(key);
+    }
+  }
+  redisClient.mget(keyList, function(err, res) {
+    for(var y = 0; y < gridDimensions; y++) {
+      for(var x = 0; x < gridDimensions; x++) {
+        var index = (gridDimensions * y) + x;
+        if (res[index] !== null) {
+          console.log(y + '-' + x);
+          console.log(res[index]);
+          self.gridMatrix[y][x].color = res[index];
+        }
+      }
+    }
+  });
 };
 
 module.exports = Grid;
