88714ba6d4029ffd14e6125a0eb64f352afb877e
Ridwan Hoq <ridwanhoq@gmail.com>
2014-03-02 14:34:06 -0600

    Removed updateGridMatrix unit test since method doesn't exist anymore (#66)

--- test/spec/server/grid.js
+++ test/spec/server/grid.js
@@ -18,24 +18,4 @@ describe('Grid', function() {
       assert(grid.gridMatrix[0].length === gridDimensions);
     });
   });
-
-  describe('updateGridMatrix', function() {
-    var grid = new Grid(gridDimensions);
-    it('should update gridMatrix with new values', function() {
-      var data = {
-        row: 0,
-        col: 0,
-        color: '#fff'
-      };
-      var gridCol = grid.gridMatrix[data.row][data.col];
-      assert(gridCol.color === '');
-
-      grid.updateGridMatrix(data);
-      assert(gridCol.color === data.color);
-
-      grid.updateGridMatrix(data);
-      assert(gridCol.color === '');
-    });
-  });
-
 });
