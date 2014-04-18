a16ca9fa3d909caebb82bd35f7318b6bc3f3b0c8
Ridwan Hoq <ridwanhoq@gmail.com>
2014-04-18 03:38:26 -0500

    right clicking now changes your color to the color that your right clicked on

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -54,8 +54,41 @@ angular.module('proGridApp')
       socket.emit('clicked', { row: row, col: col, color: userColor, apiKey: apiKey });
     };
 
+    $scope.gridRightClicked = function(row, col) {
+      var selector = '.col_' + row + '_' + col;
+      var el = document.querySelector(selector);
+      userColor = colorToHex(el.style.backgroundColor);
+    };
+
     $scope.closeMessage = function() {
       $scope.message = false;
     };
     $scope.gridClicked = _.throttle($scope.gridClicked, 100, {trailing: false});
   }]);
+
+angular.module('proGridApp')
+  .directive('ngRightClick', function($parse) {
+      return function(scope, element, attrs) {
+          var fn = $parse(attrs.ngRightClick);
+          element.bind('contextmenu', function(event) {
+              scope.$apply(function() {
+                  event.preventDefault();
+                  fn(scope, {$event:event});
+                });
+            });
+        };
+    });
+
+function colorToHex(color) {
+  if (color.substr(0, 1) === '#') {
+    return color;
+  }
+  var digits = /(.*?)rgb\((\d+), (\d+), (\d+)\)/.exec(color);
+
+  var red = parseInt(digits[2]);
+  var green = parseInt(digits[3]);
+  var blue = parseInt(digits[4]);
+
+  var rgb = blue | (green << 8) | (red << 16);
+  return digits[1] + '#' + rgb.toString(16);
+}
