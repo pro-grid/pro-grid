ce2663c5ccbc5a78f752e04ca4f81e022010e2c2
Ridwan Hoq <ridwanhoq@gmail.com>
2014-07-02 03:52:07 -0500

    added updateGrid tests that don't touch redis

--- test/grid.js
+++ test/grid.js
@@ -4,10 +4,21 @@ var assert = require('assert')
   , Grid = require('../lib/grid')
   , redis = require('redis');
 
+var returnsTrue = function() {
+  return true;
+};
+
 describe('Grid', function() {
   var gridDimensions = 32;
+
   var redisClient = redis.createClient();
   redisClient.select(15);
+
+  var client = {
+    broadcast: {
+      emit: returnsTrue,
+    }
+  };
 
   beforeEach(function() {
     redisClient.flushdb();
@@ -34,6 +45,33 @@ describe('Grid', function() {
       redisClient.get('gridSaved', function(err, reply) {
         assert(reply);
       });
+    });
+  });
+
+  describe('updateGrid', function() {
+    it('should update grid with color if no color present', function() {
+      var data = {
+        row: '12',
+        col: '24',
+        color: '#FFFFFF'
+      };
+      var grid = new Grid(gridDimensions, redisClient);
+      assert(grid.gridMatrix[data.row][data.col].color === '');
+      grid.updateGrid(client, data);
+      assert(grid.gridMatrix[data.row][data.col].color === data.color);
+    });
+
+    it('should update grid by deleting color if a color is already present', function() {
+      var data = {
+        row: '12',
+        col: '24',
+        color: '#FFFFFF'
+      };
+      var grid = new Grid(gridDimensions, redisClient);
+      grid.updateGrid(client, data);
+      assert(grid.gridMatrix[data.row][data.col].color === data.color);
+      grid.updateGrid(client, data);
+      assert(grid.gridMatrix[data.row][data.col].color === '');
     });
   });
 });
