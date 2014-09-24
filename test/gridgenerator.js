bdf3054700a3fc483611ebb2c5fb8549ff55bb1f
Ridwan Hoq <ridwanhoq@gmail.com>
2014-09-24 01:00:34 -0500

    add test for gridgenerator.js

---
+++ test/gridgenerator.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var assert = require('assert')
+  , gridGenerator = require('../lib/gridGenerator');
+
+describe('gridGenerator', function() {
+  it('should generate a correctly size 2d array', function() {
+    var dimensions = 32;
+    var gm = gridGenerator.gridGenerator(dimensions);
+    assert(gm.length === dimensions);
+    assert(gm[0].length === dimensions);
+    assert(gm[0][0].hasOwnProperty('row'));
+    assert(gm[0][0].hasOwnProperty('col'));
+    assert(gm[0][0].hasOwnProperty('color'));
+  });
+});
